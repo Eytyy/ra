44f32b02eee898c187107100515c6d8edc058eeb
eytyy <e.tayyem@gmail.com>
2023-06-06 16:44:40 +0300

    group events by date

--- src/components/Shows.tsx
+++ src/components/Shows.tsx
@@ -26,39 +26,55 @@ export default function Shows({ update, events }: Props) {
     toggleList(!visible);
   };
 
-  // Order calendr events by date and remove duplicates
-  const eventsByName = useMemo(() => {
-    const sorted = events
-      // remove events without a summary or start date
-      .filter((event) => {
-        if (!event.summary || !event.start?.dateTime) return false;
-        return event.summary.trim() !== '';
-      })
-      // sort by date
-      .sort((a, b) => {
-        const dateA = a.start?.dateTime as string;
-        const dateB = b.start?.dateTime as string;
-        if (dateA < dateB) return -1;
-        if (dateA > dateB) return 1;
-        return 0;
-      });
-    // return only unique events
-    return sorted.filter(
-      (event, index, self) =>
-        index ===
-        self.findIndex(
-          (e) => e.summary!.trim() === event.summary!.trim()
+  // Order calendar events by date and remove duplicates
+  const filteredEvents = useMemo(() => {
+    return (
+      events
+        // remove events without a summary or start date
+        .filter((event) => {
+          if (!event.summary || !event.start?.dateTime) return false;
+          return event.summary.trim() !== '';
+        })
+        // sort by date (descending)
+        .sort((a, b) => {
+          const dateA = a.start?.dateTime as string;
+          const dateB = b.start?.dateTime as string;
+          if (dateA < dateB) return -1;
+          if (dateA > dateB) return 1;
+          return 0;
+        })
+        // remove duplicate events
+        .filter(
+          (event, index, self) =>
+            index ===
+            self.findIndex(
+              (e) => e.summary!.trim() === event.summary!.trim()
+            )
         )
     );
   }, [events]);
 
   // Filter calendar events based on search value
   const results = useMemo(() => {
-    if (value == '') return eventsByName;
-    return eventsByName.filter((event) =>
+    if (value == '') return filteredEvents;
+    return filteredEvents.filter((event) =>
       event.summary!.toLowerCase().includes(value.toLowerCase())
     );
-  }, [eventsByName, value]);
+  }, [filteredEvents, value]);
+
+  // Group events by date
+  const resultsByDate = useMemo(() => {
+    return results.reduce((acc, event) => {
+      const date = event.start?.dateTime?.split('T')[0];
+      if (!date) return acc;
+
+      if (!acc[date]) {
+        acc[date] = [];
+      }
+      acc[date].push(event);
+      return acc;
+    }, {} as Record<string, CalendarEvent[]>);
+  }, [results]);
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     setValue(e.target.value);
@@ -72,6 +88,18 @@ export default function Shows({ update, events }: Props) {
     toggle();
     setValue('');
   }
+
+  const formatGroupDate = (date: string) => {
+    const formattedDate = new Date(date).toLocaleDateString(
+      'default',
+      {
+        weekday: 'long',
+        month: 'long',
+        day: '2-digit',
+      }
+    );
+    return formattedDate;
+  };
 
   return (
     <div
@@ -108,14 +136,23 @@ export default function Shows({ update, events }: Props) {
       </div>
 
       {visible && (
-        <div className="space-y-4">
-          {results.map((event) => (
-            <div
-              key={event.id}
-              className="hover:bg-[#111010] cursor-pointer bg-[#202020] p-4 rounded-full text-center"
-              onClick={() => updateSelected(event)}
-            >
-              {event.summary}
+        <div className="space-y-6">
+          {Object.entries(resultsByDate).map(([date, events]) => (
+            <div key={date} className="space-y-2">
+              <h3 className="text-xl text-gray-400">
+                {formatGroupDate(date)}
+              </h3>
+              <div className="space-y-4">
+                {events.map((event) => (
+                  <button
+                    key={event.id}
+                    className="hover:bg-[#111010] cursor-pointer bg-[#202020] p-4 rounded-full text-center w-full"
+                    onClick={() => updateSelected(event)}
+                  >
+                    {event.summary}
+                  </button>
+                ))}
+              </div>
             </div>
           ))}
         </div>
